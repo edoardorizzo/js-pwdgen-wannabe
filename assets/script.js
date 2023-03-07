42d77c002c4820ffd616261028e4337f056f9146
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-07 15:30:48 +0100

    change let with const

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
 // Chiedi all'utente il suo nome,
-let userName = prompt('Inserisci il tuo nome');
+const userName = prompt('Inserisci il tuo nome');
 console.log(userName);
 
 // poi chiedi il suo cognome,
-let userSurname = prompt ('Inserisci il tuo cognome');
+const userSurname = prompt ('Inserisci il tuo cognome');
 console.log(userSurname);
 
 // poi chiedi il suo colore preferito
-let color = prompt ('Insersci il tuo colore preferito');
+const color = prompt ('Insersci il tuo colore preferito');
 console.log(color);
 
 // Genera un numero casuale tra 1 e 99
-let randomNumber = Math.floor(Math.random() * 99) + 1;
+const randomNumber = Math.floor(Math.random() * 99) + 1;
 
 // Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
 const psw = (userName.toLowerCase() + userSurname.toLowerCase() + color.toLowerCase() + randomNumber);
