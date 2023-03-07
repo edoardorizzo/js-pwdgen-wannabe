7e738067cc669833a92499e876d8e39f057dc7b7
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-07 14:49:26 +0100

    Add final script

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
-alert('ciao')
 // Chiedi all'utente il suo nome,
-// poi chiedi il suo cognome,
-// poi chiedi il suo colore preferito
-// Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+let userName = prompt('Inserisci il tuo nome');
+console.log(userName);
 
+// poi chiedi il suo cognome,
+let userSurname = prompt ('Inserisci il tuo cognome');
+console.log(userSurname);
+
+// poi chiedi il suo colore preferito
+let color = prompt ('Insersci il tuo colore preferito');
+console.log(color);
+
+// Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+const psw = (userName + userSurname + color + 23);
+console.log(psw);
+
+document.getElementById('new_psw').innerHTML = (psw)
+console.log(document.getElementById('new_psw'));
